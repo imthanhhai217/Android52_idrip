37b73820e6e926c98d78a7558c6a0f32b0163573
quachhuyduc <quachhuyduc953@gmail.com>
2023-06-30 16:13:20 +0700

    pushhh

--- app/src/main/java/com/vndevpro/android52_idrip/ui/fragements/WishFragment.kt
+++ app/src/main/java/com/vndevpro/android52_idrip/ui/fragements/WishFragment.kt
@@ -15,30 +15,28 @@ import com.vndevpro.android52_idrip.models.Product
 import com.vndevpro.android52_idrip.ui.MainActivity
 import com.vndevpro.android52_idrip.ui.viewmodels.WishListViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class WishFragment : Fragment() {
+    lateinit var binding:FragmentWishBinding
 
-    private val TAG = "WishFragment"
-
-    lateinit var binding: FragmentWishBinding
     lateinit var wishListViewModel: WishListViewModel
+
     lateinit var mProductAdapter: ListProductAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_wish, container, false)
         binding = FragmentWishBinding.bind(view)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -46,11 +44,14 @@ class WishFragment : Fragment() {
         wishListViewModel = (activity as MainActivity).wishListViewModel
 
         initView()
+
+
+
         wishListViewModel.getAllWishList().observe(viewLifecycleOwner, Observer {
-            Log.d(TAG, "onViewCreated: " + it.size)
             it?.let {
                 bindData(it)
             }
+
         })
     }
 
@@ -58,24 +59,28 @@ class WishFragment : Fragment() {
         mProductAdapter.updateData(it)
     }
 
+
     private fun initView() {
         mProductAdapter = ListProductAdapter(callback)
         binding.rvWishList.apply {
             adapter = mProductAdapter
-            layoutManager = GridLayoutManager(activity, 2)
+            layoutManager = GridLayoutManager(activity,2)
         }
     }
-
-    private val callback = object : ListProductAdapter.IClickListener {
+    private val callback = object :ListProductAdapter.IClickListener{
         override fun changeWishStateListener(position: Int) {
             val product = mProductAdapter.differ.currentList[position]
-            if (product.isWish) {
+            if(product.isWish){
                 wishListViewModel.deleteWish(product)
             }
+
         }
 
         override fun showDetailsProductListener(position: Int) {
+            TODO("Not yet implemented")
         }
+
+
     }
 
 }
